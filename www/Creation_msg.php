4a199086e78ed1c69e9c9d29aa4d63f93952f91a
Lilian Drets <62050994+LilianDrets@users.noreply.github.com>
2021-01-20 11:56:49 +0100

    Zone d'écriture + parcourir pour les images

---
+++ www/Creation_msg.php
@@ -0,0 +1,10 @@
+<?php
+echo '
+	<form action="insertion_db.php" method="post" enctype="multipart/form-data">', PHP_EOL,
+	'</t><label for="message">Message:</label><br>', PHP_EOL,
+	'</t><input type="text" name="msg" placeholder="Ecrit ton message ici...">', PHP_EOL,
+	'</t><input type="file" name="img" accept=".png, .jpg, .jpeg, .gif"><br/>', PHP_EOL,
+	'</t><input type="submit" value="Envoyer">', PHP_EOL,
+	'</form>', PHP_EOL
+?>
+	

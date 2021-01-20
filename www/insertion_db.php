4bd27cb90a6712e294db8ce3a79ea586fa0cb25f
Lilian Drets <62050994+LilianDrets@users.noreply.github.com>
2021-01-20 11:54:31 +0100

    Ajout du chemin de l'image dans la base et création du répertoire contenant l'image

--- www/insertion_db.php
+++ www/insertion_db.php
@@ -2,8 +2,6 @@
 
     include_once "assets/php/utils.inc.php";
 
-
-
     // Création ou restauration de la session
     session_start();
 
@@ -18,14 +16,39 @@
     mysqli_select_db($dbLink , 'romain-ayme_vanestarre')
         or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
 
-    //    $msg = $_POST['msg'];
-    //    $img = $_POST['img'];
-
-    $msg = 'salutation ! ßcoucou ßcovid_19 comment ca va ? ßcanard';
-    $img = NULL;
+    $msg = $_POST['msg'];
 
     $id_msg = insert_msg_db($_SESSION['user_id'], $msg, $img, $dbLink);
 
-    manage_tag($msg, $dbLink, $id_msg);
+   manage_tag($msg, $dbLink, $id_msg);
 
-    header('Location: index.php');
+	$repertoireDestination = 'assets/utilisateurs/', $_SESSION['user_id'] ,'/img_msg/,' $id_msg ,'/';
+	$nomDestination = nommage($_FILES["msg"]["type"]);
+
+	if (is_uploaded_file($_FILES["img"]["tmp_name"])) {
+		if (rename($_FILES["img"]["tmp_name"], $repertoireDestination.$nomDestination)) {
+			echo 'Fichier uploadé';
+		}
+		else {
+			echo 'Erreur dans le déplacement du fichier dans le serveur';
+		}
+	}
+	else {
+		echo 'Erreur dans l'uploadage (terme approximatif) du fichier';
+	}
+
+	$query = 'INSERT INTO messages (IMG) VALUES (\'', $repertoireDestination, $nomDestination, '\')';
+	execute_query($dbLink, $query);
+	
+	$nom = $repertoireDestination, $nomDestination;
+
+    	if (is_dir($nom)) {
+                      echo 'Le message existe déjà';  
+                      }
+    else { 
+          mkdir($nom);
+          }
+?>
+   header('Location: index.php');
+
+?>

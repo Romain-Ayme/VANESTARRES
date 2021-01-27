6cb39e92dc696572f0dcf89bb48062789fd894d5
Romain Ayme <49228057+Romain-Ayme@users.noreply.github.com>
2021-01-27 16:41:05 +0100

    Delete www directory

--- www/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Création ou restauration de la session
-session_start();
-// Suppression de la session
-session_destroy();
-// Redirection vers la page : login.php
-header('Location: index.php');

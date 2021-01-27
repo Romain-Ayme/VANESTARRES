1a89a8a0d541bd11babc63acc07ea080c17b85d1
Romain Ayme <49228057+Romain-Ayme@users.noreply.github.com>
2021-01-27 20:18:49 +0100

    Delete www directory

--- www/assets/js/js.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function()
-{
-    window.onscroll = function(ev)
-    {
-        document.getElementById("scrollUp").className = (window.pageYOffset > 100) ? "visible" : "invisible";
-    };
-});
--- www/assets/php/Logout_Process.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Création ou restauration de la session
-session_start();
-// Suppression de la session
-session_destroy();
-// Redirection vers la page : index.php
-header('Location: ../../index.php');
